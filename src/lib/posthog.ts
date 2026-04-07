d9e7d7c34ec86bb3b0547cd102d8478aae6b1e02
Nihal <nihal.te@gmail.com>
2026-04-07 23:24:05 +0400

    Update posthog.ts

--- src/lib/posthog.ts
+++ src/lib/posthog.ts
@@ -8,6 +8,7 @@ export function initPostHog() {
 
   posthog.init(key, {
     api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST ?? 'https://us.i.posthog.com',
+    ui_host: process.env.NEXT_PUBLIC_POSTHOG_UI_HOST ?? 'https://us.posthog.com',
     person_profiles: 'identified_only',
     capture_pageview: false, // We handle this manually with the PageViewTracker
     capture_pageleave: true,
